5e16558ba07802c68aa7e2954e5fb69b691ea59d
LKochan123 <kochanczyk.lukasz@gmail.com>
2024-05-28 00:43:46 +0200

    Combining all parts of project

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from messages import find_messages_dependency
+import times as t
+
+
+def load_file(file_name: str):
+    logs = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            logs.append(line.strip())
+
+    return logs
+
+
+if __name__ == '__main__':
+    # Constants
+    MINUTES_THRESHOLD = 15
+    DAILY_THRESHOLD = 0.8
+    MESSAGES_THRESHOLD = 0.8
+
+    # Loading file
+    file_name = './data/project_logs.txt'
+    logs = load_file(file_name)
+
+    # Times part
+    print('------------ Times part ------------')
+    message_times = t.get_message_times(logs)
+    results = t.analyze_cyclic_behaviors(message_times, MINUTES_THRESHOLD)
+    daily_patterns = t.detect_daily_patterns(results, DAILY_THRESHOLD)
+    t.show_detected_daily_patterns(daily_patterns)
+
+    # Messages part
+    print('------------ Messages part ------------')
+    find_messages_dependency(logs, MESSAGES_THRESHOLD)
